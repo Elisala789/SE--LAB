b8b25166b231f439b91da3fe8dfcbf774b3a9316
Himu chitta <himuchitta@gmail.com>
2025-03-17 03:06:00 +0530

    Himajala

--- spring-boot-backend/src/main/java/com/example/demo/model/Lab.java
+++ spring-boot-backend/src/main/java/com/example/demo/model/Lab.java
@@ -34,4 +34,35 @@ public class Lab {
 	}
 	private String name;
     private int servers;
+    private String location;
+    private String operating_hours;
+    private int ram;
+    private int processors;
+
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getOperating_hours() {
+		return operating_hours;
+	}
+	public void setOperating_hours(String operating_hours) {
+		this.operating_hours = operating_hours;
+	}
+	public int getRam() {
+		return ram;
+	}
+	public void setRam(int ram) {
+		this.ram = ram;
+	}
+	public int getProcessors() {
+		return processors;
+	}
+	public void setProcessors(int processors) {
+		this.processors = processors;
+	}
+    
+    
 }
