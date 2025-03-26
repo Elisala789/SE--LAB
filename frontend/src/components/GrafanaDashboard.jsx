923ae8de6033da6b1ab36f112ce4a66348fcaa73
Elisala789 <prazvalelisala2848@gmail.com>
2025-03-26 19:38:24 +0530

    Update GrafanaDashboard.jsx

--- frontend/src/components/GrafanaDashboard.jsx
+++ frontend/src/components/GrafanaDashboard.jsx
@@ -3,6 +3,8 @@ import Layout from "./Layout";
 
 const GrafanaDashboard = () => {
   return (
+     <Layout>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
    
   );
 };
