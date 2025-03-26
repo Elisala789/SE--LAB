a20e440e47f88e6484d049637daa529138f971cb
Himu chitta <himuchitta@gmail.com>
2025-03-27 04:03:58 +0530

    Updated EditLabSpace.jsx

--- frontend/src/components/EditLabSpace.jsx
+++ frontend/src/components/EditLabSpace.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './EditLabSpace.css';
 
-const EditLabSpace = () => {
+const EditLabSpace = ({ space, onUpdate, onCancel }) => {
+  const [labName, setLabName] = useState(space.labName);
+  const [capacity, setCapacity] = useState(space.capacity);
+
+  const handleUpdate = () => {
+    onUpdate({ ...space, labName, capacity });
+  };
+
   return (
-    <div>EditLabSpace</div>
-  )
-}
+    <div className="edit-lab-space">
+      <h3>Edit Lab Space</h3>
+      <input
+        type="text"
+        value={labName}
+        onChange={(e) => setLabName(e.target.value)}
+      />
+      <input
+        type="number"
+        value={capacity}
+        onChange={(e) => setCapacity(e.target.value)}
+      />
+      <button onClick={handleUpdate}>Update</button>
+      <button onClick={onCancel}>Cancel</button>
+    </div>
+  );
+};
 
-export default EditLabSpace
+export default EditLabSpace;
