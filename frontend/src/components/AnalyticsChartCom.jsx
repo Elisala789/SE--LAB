0ed51657fa599af5716df33882feba5f82b7f4a9
Pranhit-625 <praneethgovindasiddichenna@gmail.com>
2025-03-27 03:58:49 +0530

    Updated AnalyticsChartCom

--- frontend/src/components/AnalyticsChartCom.jsx
+++ frontend/src/components/AnalyticsChartCom.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+// src/com/AnalyticsChartCom.js
+import React from 'react';
 
-const AnalyticsChartCom = () => {
-  return (
-    <div>AnalyticsChartCom</div>
-  )
-}
+const AnalyticsChartCom = ({ data }) => {
+    return (
+        <div>
+            <h3>Analytics Chart</h3>
+            {/* Placeholder for chart */}
+            <p>Chart would be displayed here based on the data provided.</p>
+            {/* You can use a chart library to visualize the data */}
+        </div>
+    );
+};
 
-export default AnalyticsChartCom
+export default AnalyticsChartCom;
