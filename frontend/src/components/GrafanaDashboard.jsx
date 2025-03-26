c2c793928f2680757161aa340a87833241999f3e
Elisala789 <prazvalelisala2848@gmail.com>
2025-03-26 19:45:16 +0530

    Update GrafanaDashboard.jsx

--- frontend/src/components/GrafanaDashboard.jsx
+++ frontend/src/components/GrafanaDashboard.jsx
@@ -126,6 +126,19 @@ const GrafanaDashboard = () => {
       <hr class="border-t-4 border-gray-600"></hr>
 
       
+    
+      <div  className="grid grid-cols-2 gap-6 w-full max-w-screen-xl mt-3">
+       
+        <div>
+          <h3 className="text-xl font-semibold text-gray-700 mb-2">
+            LogBack Statistics
+          </h3>
+          <iframe src="http://localhost:3000/d-solo/X034JGT7Gz/springboot-apm-dashboard?orgId=1&timezone=browser&var-application=Server%20Monitoring%20&var-instance=$__all&var-hikaricp=HikariPool-1&var-memory_pool_heap=$__all&var-memory_pool_nonheap=$__all&theme=light&panelId=6&__feature.dashboardSceneSolo" width="550" height="300" frameborder="0"></iframe>
+        </div>
+
+        
+
+      
    
   );
 };
