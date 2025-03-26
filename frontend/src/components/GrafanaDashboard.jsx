942bcfea7c1cbb3fd51ef1da82862e6e2ed4fa6a
Elisala789 <prazvalelisala2848@gmail.com>
2025-03-26 19:40:22 +0530

    Update GrafanaDashboard.jsx

--- frontend/src/components/GrafanaDashboard.jsx
+++ frontend/src/components/GrafanaDashboard.jsx
@@ -25,6 +25,15 @@ const GrafanaDashboard = () => {
     
     
       <hr class="border-t-4 border-gray-600"></hr>
+    <div className="grid grid-cols-2 gap-6 w-full max-w-screen-xl mt-3 ">
+          {/* JDBC Connections */}
+        <div>
+          <h3 className="text-xl font-semibold text-gray-700 mb-2">
+            CPU Statistics
+          </h3>
+          <iframe src="http://localhost:3000/d-solo/X034JGT7Gz/springboot-apm-dashboard?orgId=1&timezone=browser&var-application=Server%20Monitoring%20&var-instance=$__all&var-hikaricp=HikariPool-1&var-memory_pool_heap=$__all&var-memory_pool_nonheap=$__all&theme=light&panelId=58&__feature.dashboardSceneSolo" width="450" height="200" frameborder="0"></iframe>
+        </div>
+    
     
     
     
